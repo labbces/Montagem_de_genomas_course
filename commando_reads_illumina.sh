1b6f162c4d928b48c904aa51752039473fe86108
jomare1188 <jomare1188@gmail.com>
2022-05-31 17:04:07 -0300

    Update commando_reads_illumina.sh

--- commando_reads_illumina.sh
+++ commando_reads_illumina.sh
@@ -4,39 +4,57 @@ cd PRATICA_ENSAMBLAGEM_DE_GENOMAS
 conda activate bbduk_env
 randomreads.sh ref=complete_genome.fasta out=PacBio.fq pacbio=t pbmin=0.001 pbmax=0.01 coverage=20 minlength=1500 maxlength=30000
 # Creo datos illumina paired end
-randomreads.sh ref=complete_genome.fasta out1=bbmap_illumina_.R1.fq out2=bbmap_illumina_R2.fq illuminanames=t paired=t coverage=20 minlength=150 maxlength=150 minq=18 maxq=40 midq=31 addpairnum=t
+randomreads.sh ref=complete_genome.fasta out1=bbmap_illumina_R1.fq out2=bbmap_illumina_R2.fq illuminanames=t paired=t coverage=20 minlength=150 maxlength=150 minq=18 maxq=40 midq=31 addpairnum=t -Xmx2g
 # bbduk limpiar
-bbduk.sh in1=bbmap_illumina_tes.R1.fq in2=bbmap_illumina_tes.R2.fq out1=bbduk/bbduk.R1.fq out2=bbduk/bbduk.R2.fq minlength=100 qtrim=w trimq=20 
-
+bbduk.sh in1=bbmap_illumina_R1.fq in2=bbmap_illumina_R2.fq out1=bbduk/bbduk.R1.fq out2=bbduk/bbduk.R2.fq minlength=100 qtrim=w trimq=20 -Xmx2g
+conda deactivate
 # FASTQ
 conda activate fastqc_env
-fastq
+fastqc
+conda deactivate
+
 # Unicycler
-conda activate unicycler
+conda activate unicycler_env
 unicycler  -1 ./bbduk/bbduk.R1.fq -2 ./bbduk/bbduk.R2.fq -o ./unicycler/
+conda deactivate
+
 # Bandage
 conda activate bandage_env
 Bandage
+conda deactivate
 
-# Mirar unycicler con datos de pacbio para ver si circulariza
-# genomescope 2.0 - jellyfish
-# Correr Flye para ensamblar reads PacBio  y ensamblaje hibrido
+# Flye
 conda activate flye_env
-flye --pacbio-hifi PacBio.fq -o ./flye -g 3.6g
+flye --pacbio-hifi PacBio.fq -o ./flye
+conda deactivate
 # Circlator
 
 # QUAST
 conda activate quast_env
 # QUAST illumina
-quast -o ./quast/illumina -r complete_genoma.fasta -g features.gff3 ./unicycler/assembly.fasta
+quast -o ./quast/illumina -r complete_genome.fasta ./unicycler/assembly.fasta
 # QUAST pacbio
-quast -o ./quast/pacbio -r complete_genoma.fasta -g features.gff3 ./flye/scaffolds.fasta 
+quast -o ./quast/pacbio -r complete_genome.fasta ./flye/assembly.fasta 
+conda deactivate
 
 # BUSCO
 conda activate busco_env
 # BUSCO illumina
 busco -i ./unicycler/assembly.fasta -o illumina -m geno --lineage rhodospirillales_odb10 --out_path ./busco
 # BUSCO pacbio
-busco -i ./flye/scaffolds.fasta -o pacbio -m geno --lineage rhodospirillales_odb10 --out_path ./busco
+busco -i ./flye/assembly.fasta -o pacbio -m geno --lineage rhodospirillales_odb10 --out_path ./busco
 
+####### UNICYCLER PARA PACBIO
+conda activate unicycler_env
+unicycler  -l PacBio.fq -o ./unicycler_pacbio/
+conda deactivate
 
+### JELLYFISH
+mkdir -p ./jellyfish/
+jellyfish count -C -m 21 -s 10000000 -t 6 bbmap_illumina_R*.fq -o ./jellyfish/reads.jf
+jellyfish histo -t 6 ./jellyfish/reads.jf > ./jellyfish/reads.histo
+
+# Unicycler hibrido
+conda activate unicycler_env
+unicycler  -1 ./bbduk/bbduk.R1.fq -2 ./bbduk/bbduk.R2.fq -l PacBio.fq -o ./unicycler_hybrid/
+conda deactivate
